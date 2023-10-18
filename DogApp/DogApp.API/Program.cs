8fa5871ecef910860072b14c2a20ae123c7f3766
Konstantin <titarenko42k@gmail.com>
2023-10-18 17:38:05 +0300

    configure api to use dal proj

--- DogApp/DogApp.API/Program.cs
+++ DogApp/DogApp.API/Program.cs
@@ -1,3 +1,5 @@
+using DogApp.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure services from class libs
+builder.Services.ConfigureDataAccessLayerServices(builder.Configuration.GetConnectionString("DogsConnectionString")!);
+
 
 var app = builder.Build();
 
